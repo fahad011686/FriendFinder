7648a3543f54f17541c7281e62454837808e15f9
Fahad <fahad011686@gmail.com>
2019-10-14 18:39:17 -0500

    Updated survey questions.

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var path = require("path");
 var express = require("express");
+var exphbs = require("express-handlebars");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
@@ -13,6 +14,11 @@ require("./routing/htmlRoutes")(app);
 app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "./public/home.html"));
 });
+
+app.get("/survey", function(req, res) {
+  res.sendFile(path.join(__dirname, "./public/survey.html"));
+});
+
 
 app.listen(PORT, function() {
     console.log("Server listening on: http://localhost:" + PORT);
